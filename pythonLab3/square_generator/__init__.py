68aa583a853cbbb73d587290026a5e205c268b8c
linhbuitung <linh.buitung18@gmail.com>
2024-03-19 23:42:44 +0100

    add task 9

--- pythonLab3/square_generator/__init__.py
+++ pythonLab3/square_generator/__init__.py
@@ -6,6 +6,10 @@ class SquareGenerator:
         return [x * x for x in (range(start, end+1))]
 
 class CubicGenerator(SquareGenerator):
+    def e_squares(self, start, end):
+        if start > end:
+            raise ValueError("Start of range cannot be greater than end.")
+        return super().e_squares(start, end)
     def e_cubes(self, start, end):
         if end < start:
             print("Error: End of range is less than start.")
