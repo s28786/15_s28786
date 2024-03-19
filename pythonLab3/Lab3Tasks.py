49ed50a901a1601098170819772f761340dd66d3
linhbuitung <linh.buitung18@gmail.com>
2024-03-19 23:31:03 +0100

    add task 4

--- pythonLab3/Lab3Tasks.py
+++ pythonLab3/Lab3Tasks.py
@@ -1,6 +1,15 @@
+import math
+
+
 class SquareGenerator:
     def e_squares(self, start, end):
+        if end < start:
+            return
         return [x * x for x in (range(start, end+1))]
 
 S = SquareGenerator()
-print(S.e_squares(1,10))
+square_list = S.e_squares(11,10)
+print(square_list)
+
+square_root = [math.sqrt(x) for x in S.e_squares(1,10)]
+print(square_root)
